cbf8f1d93df60448bd9f82a10c85a6a6cf95b35d
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-11-17 18:56:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-// client/src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import ImageUpload from './components/imageUpload';
+import ImageViewer from './components/imageViewer';
+import BiometricUnlock from './components/biometricUnlock';
+import BiometricSettings from './components/biometricSettings';
+import LoginForm from './components/loginForm';
+import LogoutButton from './components/logoutButton';
+import MatchHistory from './components/matchHistory';
+import RegisterForm from './components/registerForm';
+import RequestResetForm from './components/requestResetForm';
+import ResetPasswordForm from './components/resetPasswordForm';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />}/>
+      <Route path="/" element={<ImageUpload/>} />
+      <Route path="/" element={<ImageViewer/>} />
+      <Route path="/" element={<BiometricUnlock />}/>
+      <Route path="/" element={<BiometricSettings/>} />
+      <Route path="/" element={<LoginForm />}/>
+      <Route path="/" element={<LogoutButton />}/>
+      <Route path="/" element={<MatchHistory />}/>
+      <Route path="/" element={<RegisterForm />}/>
+      <Route path="/" element={<RequestResetForm />}/>
+      <Route path="/" element={<ResetPasswordForm />}/>
+  </Routes>
+  </Router>
   );
 }
 
