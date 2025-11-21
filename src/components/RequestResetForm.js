d0659b626e3390771976d026e7175328ba61246c
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-11-21 12:39:06 +0100

    Update RequestResetForm.js

--- src/components/RequestResetForm.js
+++ src/components/RequestResetForm.js
@@ -1,6 +1,5 @@
 // client/src/components/RequestResetForm.js
 import { View, Text } from 'react-native-web';
-import { View, Text, TouchableOpacity } from 'react-native';
 function RequestResetForm() {
   const [email, setEmail] = useState('');
   const handleRequest = async () => {
