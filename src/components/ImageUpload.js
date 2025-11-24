f71d41e29418486e11ac009607ee3762a0ad27f9
ohidans <74150381+danielwealth@users.noreply.github.com>
2025-11-24 15:47:15 +0100

    Update ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -17,7 +17,7 @@ export default function ImageUpload() {
     formData.append('passcode', passcode);
 
     try {
-      const resp = await fetch(`${process.env.REACT_APP_API_URL}/upload`, {
+      const resp = await fetch(`${process.env.REACT_APP_API_URI}/upload`, {
         method: 'POST',
         body: formData,
       });
