3f7ca6d9b0ec71dd0ccaf877a28112fb426487f2
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-04-11 00:17:07 +0100

    Update UserDashboard.js

--- src/components/user/UserDashboard.js
+++ src/components/user/UserDashboard.js
@@ -1,8 +1,9 @@
 // client/src/components/user/UserDashboard.js
+{ "compilerOptions": { "baseUrl": "src" } }
 import React, { useEffect, useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native-web';
 import LogoutButton from '../LogoutButton';
-import { getToken } from '../../services/auth';
+import { getToken } from 'services/auth';
 
 export default function UserDashboard({ setView }) {
   const [userEmail, setUserEmail] = useState(null);
