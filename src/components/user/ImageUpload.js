9e97b6408cb4096d48cdae3c864e8e62428cbdbb
ohidans <74150381+danielwealth@users.noreply.github.com>
2026-04-10 23:51:37 +0100

    Update ImageUpload.js

--- src/components/user/ImageUpload.js
+++ src/components/user/ImageUpload.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native-web';
 import { getFaceDescriptor, areModelsReady } from '../../faceApiHelpers';
 import { postDocumentWithDescriptor } from '../../services/verify';
-import { getToken } from '../services/auth';
+import { getToken } from '../../services/auth';
 
 export default function ImageUpload({ setView }) {
   const [file, setFile] = useState(null);
